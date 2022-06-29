36a1a676b7f45992fc9741e236d741d148d28b1a
SenbonzakuraKageyoshi <kacceldev@gmail.com>
2022-06-29 13:58:44 +0300

    Стилизована страцина профиля, сделан динамический вывод данных текущего юзреа

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import UserImg from '../../images/profile/avatar.png'
+import UserImg from '../../images/profile/avatar.png';
+import { useSelector } from 'react-redux';
 import './sidebar.css'
 
 const Sidebar = () => {
-    return (
-        <aside className="sidebar">
-            <Link to="/profile" className="profile-link">
-                <img src={UserImg} alt="" className="user-img" width={40} height={40}/>
-                <h1 className="user-name">Mark</h1>
-            </Link>
-            <nav className="sidebar-nav">
-                <ul className="sidebar-nav__list">
-                    <li className="sidebar-nav__list-item"> 
-                        <Link to="/my-posts" className='sidebar-nav__list-link'>My posts</Link>
-                    </li>
-                    <li className="sidebar-nav__list-item"> 
-                        <Link to="/add-post" className='sidebar-nav__list-link'>Add posts</Link>
-                    </li>
-                    <li className="sidebar-nav__list-item"> 
-                        <Link to="/posts" className='sidebar-nav__list-link'>Posts</Link>
-                    </li>
-                    <li className="sidebar-nav__list-item"> 
-                        <Link to="/liked-posts" className='sidebar-nav__list-link'>Liked posts</Link>
-                    </li>
-                </ul>
-            </nav>
-        </aside>
-    );
+
+    const {data, loading} = useSelector(state => state.auth);
+
+    if(loading === 'loaded' && data !== null){
+        return (
+            <aside className="sidebar">
+                <Link to="/profile" className="profile-link">
+                    <img src={UserImg} alt="" className="user-img" width={40} height={40}/>
+                    <h1 className="user-name">{data.name}</h1>
+                </Link>
+                <nav className="sidebar-nav">
+                    <ul className="sidebar-nav__list">
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/my-posts" className='sidebar-nav__list-link'>My posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/add-post" className='sidebar-nav__list-link'>Add posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/posts" className='sidebar-nav__list-link'>Posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/liked-posts" className='sidebar-nav__list-link'>Liked posts</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        );
+    }else{
+        return (
+            <aside className="sidebar">
+                <p>loading</p>
+                <nav className="sidebar-nav">
+                    <ul className="sidebar-nav__list">
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/my-posts" className='sidebar-nav__list-link'>My posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/add-post" className='sidebar-nav__list-link'>Add posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/posts" className='sidebar-nav__list-link'>Posts</Link>
+                        </li>
+                        <li className="sidebar-nav__list-item"> 
+                            <Link to="/liked-posts" className='sidebar-nav__list-link'>Liked posts</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        );
+    }
 }
 
 export default Sidebar;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
-import UserImg from '../../images/profile/avatar.png'
+import UserImg from '../../images/profile/avatar.png';
 import './profile.css';
 
 const Profile = () => {
-    return (
-        <section className="profile">
-            <Sidebar />
-            <div className="profile__content">
-                <img src={UserImg} alt="" className="user-img-big" width={200} height={200}/>
-            </div>
-        </section>
-    );
+
+    const {data, loading} = useSelector(state => state.auth);
+
+    if(loading === 'loaded' && data !== null){
+        return (
+            <section className="profile">
+                <Sidebar /> 
+                <div className="profile__content">
+                    <div className="user-info">
+                        <img src={UserImg} alt="" className="user-img-big" width={200} height={200}/>
+                        <h1 className="user-name">{data.name}</h1>
+                        <h2 className="user-email">{data.email}</h2>
+                    </div>
+                </div>
+            </section>
+        );
+    }else{
+        return (
+            <section className="profile">
+                <Sidebar />
+                <div className="profile__content">
+                    <p>loading</p>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Profile;
